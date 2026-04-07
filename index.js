84408e08ea6147dc55a21a63efa518bfd207b588
Dedeepya Korukonda <korukondadedeepya2002@gmail.com>
2026-04-07 17:41:41 +0930

    Manual CORS headers

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ const axios = require('axios');
 const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
-app.use(cors({
-  origin: '*',
-  credentials: false
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(express.json());
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
